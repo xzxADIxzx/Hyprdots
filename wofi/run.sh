f93770a8f47388dde148e9cb8d5e9af7fad68b02
xzxADIxzx <76255437+xzxADIxzx@users.noreply.github.com>
2025-12-22 22:08:45 +0300

    upload runnables for wofi

---
+++ wofi/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+function mod
+{
+    if pidof -q hypridle
+    then
+        cat $1
+    else
+        sed -e "s/Inhibit/Catalyze/" $1
+    fi
+}
+
+wofi="$1 --define dmenu-print_line_num=true"
+path="$2"
+
+num=$(mod $path-names | $wofi || echo -1)
+cmd=$(sed -n "$(($num + 1))p" $path-execs)
+
+hyprctl dispatch exec $cmd
